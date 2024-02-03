f14a277b8537364f05bb6afa8021d73030bd7619
chan-hyeok-kim <132668682+chan-hyeok-kim@users.noreply.github.com>
2024-02-03 19:37:12 +0900

    Update SendMessage.java

--- src/main/java/com/ham/main/util/SendMessage.java
+++ src/main/java/com/ham/main/util/SendMessage.java
@@ -22,46 +22,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 public class SendMessage {
 	
-	@SuppressWarnings("unchecked")
-	public void sendMsg(String tel, String rand) {
-        // 호스트 URL
-        String hostNameUrl = "https://sens.apigw.ntruss.com";
-        // 요청 URL
-        String requestUrl= "/sms/v2/services/";
-        // 요청 URL Type
-        String requestUrlType = "/messages";
-        // 개인 인증키
-        String accessKey = "YOBMQxoPXU3lqd6dYi34";
-        // 2차 인증을 위해 서비스마다 할당되는 service secret
-        String secretKey = "tG7ZtjRbTIh4EVH4MQTEMLHx53OC0eL2OTuruv9R";
-        // 프로젝트에 할당된 SMS 서비스 ID
-        String serviceId = "ncp:sms:kr:313932845338:gdj68_kitchen";
-        // 요청 method
-        String method = "POST";
-        // current timestamp (epoch)
-        String timestamp = Long.toString(System.currentTimeMillis());
-        requestUrl += serviceId + requestUrlType;
-        String apiUrl = hostNameUrl + requestUrl;
-
-        // JSON 을 활용한 body data 생성
-        JSONObject bodyJson = new JSONObject();
-        JSONObject toJson = new JSONObject();
-        JSONArray  toArr = new JSONArray();
-        
-        // 난수와 함께 전송
-        //toJson.put("content","ILikeKitchen 본인인증 ["+rand+"]");		
-        toJson.put("to",tel);
-        toArr.add(toJson);
-	    
-        // 메시지 Type (sms | lms)
-        bodyJson.put("type","sms");
-        bodyJson.put("contentType","COMM");
-        bodyJson.put("countryCode","82");
-        bodyJson.put("content", "ILikeKitchen ["+rand+"]");
-        
-        // 발신번호 * 사전에 인증/등록된 번호만 사용할 수 있습니다.
-        bodyJson.put("from","01026459730");		
-        bodyJson.put("messages", toArr);		
+	
+	
 	    
         String body = bodyJson.toJSONString();
 	    
