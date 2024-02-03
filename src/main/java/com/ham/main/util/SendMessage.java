7c4949b0517f92d395e37d2e861572ef179fb71f
chan-hyeok-kim <132668682+chan-hyeok-kim@users.noreply.github.com>
2024-02-03 19:04:48 +0900

    Update SendMessage.java

--- src/main/java/com/ham/main/util/SendMessage.java
+++ src/main/java/com/ham/main/util/SendMessage.java
@@ -31,9 +31,9 @@ public class SendMessage {
         // 요청 URL Type
         String requestUrlType = "/messages";
         // 개인 인증키
-        String accessKey = "YOBMQxoPXU3lqd6dYi34";
+        String accessKey = "";
         // 2차 인증을 위해 서비스마다 할당되는 service secret
-        String secretKey = "tG7ZtjRbTIh4EVH4MQTEMLHx53OC0eL2OTuruv9R";
+        String secretKey = "";
         // 프로젝트에 할당된 SMS 서비스 ID
         String serviceId = "ncp:sms:kr:313932845338:gdj68_kitchen";
         // 요청 method
